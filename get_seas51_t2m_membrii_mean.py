e46ba9b7eed7e3e88e3fe582a843a7ea715c449a
mihaela-neacsu <mihaela.ridichie@gmail.com>
2026-06-29 18:06:25 +0300

    Add files via upload

---
+++ get_seas51_t2m_membrii_mean.py
@@ -0,0 +1,26 @@
+import cdsapi
+
+dataset = "seasonal-monthly-single-levels"
+request = {
+    "originating_centre": "ecmwf",
+    "system": "51",
+    "variable": ["2m_temperature"],
+    "product_type": ["monthly_mean"],
+    "year": ["YY"],
+    "month": ["MM"],
+    "leadtime_month": [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6"
+    ],
+    "data_format": "grib",
+    "area": [75, -25, 30, 60],
+    "grid":  [0.25,0.25]
+    }
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
+
